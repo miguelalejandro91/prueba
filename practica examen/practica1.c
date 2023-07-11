2cf2c0abd5829dd7b95d2426b9b2742dbffcb8b1
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-07-11 11:08:18 -0500

    creacion

--- practica examen/practica1.c
+++ practica examen/practica1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int es_primo(int n) {
 int i;
-for(i = 2; i < n; i++) {
+for(i = 5; i < n; i++) {
 if(n % i == 0) {
 return 0;
         }
---
+++ progreso2/variable nombre.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  char nombre[] = "miguel";
+  printf("Mi nombre es %s\n", nombre);
+  return 0;
+}
